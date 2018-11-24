2d618185de87b30f07ed680fb4b33066ca030a38
泽建 <649708060@qq.com>
2018-11-24 15:56:00 +0800

    Fff

---
+++ LeetCode91/LeetCode91/leetcode744.swift
@@ -0,0 +1,27 @@
+//
+//  leetcode744.swift
+//  LeetCode91
+//
+//  Created by gaozejian on 2018/11/24.
+//  Copyright © 2018 solordreams. All rights reserved.
+//
+
+import UIKit
+
+class leetcode744: NSObject {
+    /*
+     给定一个只包含小写字母的有序数组letters 和一个目标字母 target，寻找有序数组里面比目标字母大的最小字母。
+     
+     数组里字母的顺序是循环的。举个例子，如果目标字母target = 'z' 并且有序数组为 letters = ['a', 'b']，则答案返回 'a'。
+     
+     示例:
+     
+     输入:
+     letters = ["c", "f", "j"]
+     target = "a"
+     输出: "c"
+     */
+    func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
+        return "a"
+    }
+}
