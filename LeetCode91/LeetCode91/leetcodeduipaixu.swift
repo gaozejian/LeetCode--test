e8f5cda0aa439cfd02e3a5a23a87993b33af6169
泽建 <649708060@qq.com>
2018-12-13 18:14:28 +0800

    堆排序nlogn

--- LeetCode91/LeetCode91/leetcodeduipaixu.swift
+++ LeetCode91/LeetCode91/leetcodeduipaixu.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class leetcodeduipaixu: NSObject {
-    func duipaixu(_ letter: [Int]) -> [Int] {
+    func duipaixu2(_ letter: [Int]) -> [Int] {
         var arr:[Int] = letter
         for k  in (0...letter.count - 1).reversed() {
             
-            arr = self.getBigDui(arr, k)
+            arr = self.getBigDui2(arr, k)
             arr.swapAt(0, k)
             
         }
@@ -21,7 +21,7 @@ class leetcodeduipaixu: NSObject {
         return arr
     }
     
-    func getBigDui(_ letter: [Int] ,_ countLength:Int) -> [Int] {
+    func getBigDui2(_ letter: [Int] ,_ countLength:Int) -> [Int] {
         var letters = letter
         //   [100,55,65,44,22,32,12,45,67,88,12,34,21,34,56,78]
         for i in (0...countLength/2).reversed() {
@@ -50,11 +50,47 @@ class leetcodeduipaixu: NSObject {
     }
     /*
      https://blog.csdn.net/yuzhihui_no1/article/details/44258297
-     时间复杂度计算
+     时间复杂度计算 O(nlogn)
      0 1 2 3 4 5 6 7...
      0 0 1 1 2 2 3 3...
      (1 + 2 + 3 + 4 + .... n/2)*2
-    ( 1+ n/2)/2 * n/2 * 2
+     ( 1+ n/2)/2 * n/2 * 2
      (2+n)/4 * n = n^2 + 2n  == O(n^2)
      */
+    
+    
+    func duipaixu(_ letter: [Int]) -> [Int] {
+        print(letter)
+        var arr:[Int] = letter
+        for k  in (0...((letter.count-1)/2)).reversed() {
+            arr = self.getBigDui(arr, k, letter.count - 1)
+        
+        }
+        for k  in (0...letter.count - 1).reversed() {
+            arr.swapAt(0, k)
+            arr = self.getBigDui(arr, 0,k-1)
+        }
+        return arr
+    }
+    
+    func getBigDui(_ letter: [Int] ,_ i:Int ,_ countLength:Int) -> [Int] {
+        var letters = letter
+            let rightIndex = i * 2 + 2
+            let leftIdenx = i*2 + 1
+        if leftIdenx > countLength {
+            return letters
+        }
+        var max :Int = leftIdenx
+        if rightIndex <= countLength {
+        
+            let right = letters[rightIndex]
+            let left = letters[leftIdenx]
+            max = right > left ? rightIndex :leftIdenx
+        }
+        if letters[i] < letters[max]{
+            letters.swapAt(i, max)
+            letters = self.getBigDui(letters, max, countLength)
+        }
+        return letters
+    }
 }
